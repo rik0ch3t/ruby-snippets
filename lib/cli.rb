50b194705bf9fcf8a7f1f53ee91fcfd02d8f1a65
rik0ch3t <rchett02@gmail.com>
2019-11-30 14:16:16 -0800

    add supported lang array

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,52 +5,64 @@ require 'fileutils'
 # Main CLI app
 class CLI < Thor
 
-  desc "include [language]", "Include snippets for the [language] language."
-  option :project, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
-  def include(language)
-    if language != 'ruby'
-      puts "Error: Language not supported."
+  @@supported_lang = {
+    "ruby" => ["thor", "metasploit"],
+    "python" => [],
+    "rust" => [],
+  }
+
+  desc "include", "Include snippets into the current workspace."
+  option :lang, :aliases => ["-l"], :type => :string, :required => true, :desc => "The language specific snippets to use."
+  option :proj, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
+  def include()
+    if !@@supported_lang.has_key?(options[:lang])
+      puts "Error: '#{options[:lang].capitalize}' not supported."
       return 1
     end
 
-    if options[:project].nil?
-      puts "Error: Project type not provided."
+    if !@@supported_lang[options[:lang]].include? options[:proj]
+      puts "Error: '#{options[:proj].capitalize}' snippets unavailable."
       return 1
     end
 
     # creat .vscode if it does not exist
     vscode_path = File.join(Dir.pwd, ".vscode")
     if !vscode_path
+      puts "Creating directory `.vscode` at #{vscode_path}..."
       Dir.mkdir(File.join(Dir.pwd, ".vscode"))
     end
 
     # copy selected projects snippets to .vscode
-    snip_files_path = File.join(Dir.pwd, "projects/#{options[:project]}/.")
+    snip_files_path = File.join(Dir.pwd, "projects/#{options[:proj]}/.")
+    puts "Adding files to the `.vscode` directory..."
     FileUtils.cp_r(snip_files_path, vscode_path)
+
+    puts "Done."
   end
 
-  desc "exclude [language]", "Deactivate snippets for the [language] language."
-  option :project, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
-  def exclude(language)
-    if language != 'ruby'
+  desc "include", "Include snippets into the current workspace."
+  option :lang, :aliases => ["-l"], :type => :string, :required => true, :desc => "The language specific snippets to use."
+  option :proj, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
+  def exclude()
+    if options[:lang] != 'ruby'
       puts "Error: Language not supported."
       return 1
     end
 
-    if options[:project].nil?
+    if options[:proj].nil?
       puts "Error: Project type not provided."
       return 1
     end
 
     # creat .vscode if it does not exist
-    vscode_path = File.join(Dir.pwd, ".vscode/#{options[:project]}.code-snippets")
+    vscode_path = File.join(Dir.pwd, ".vscode/#{options[:proj]}.code-snippets")
     if !vscode_path
       puts "Error: .vscode does not exist."
       return 1
     end
 
     # copy selected projects snippets to .vscode
-    snip_files_path = File.join(Dir.pwd, "projects/#{options[:project]}/.")
+    snip_files_path = File.join(Dir.pwd, "projects/#{options[:proj]}/.")
     FileUtils.rm_r(vscode_path)
   end
 end
