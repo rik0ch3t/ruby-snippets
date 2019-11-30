308b17271507d5b832a9138fc9f859419612ffee
rik0ch3t <rchett02@gmail.com>
2019-11-29 16:25:40 -0800

    remove test command

--- lib/snippets.rb
+++ lib/snippets.rb
@@ -3,12 +3,6 @@ require 'thor'
 
 module Snippets
   class CLI < Thor
-    
-    desc "test [word]", "description for the command"
-    option :seven, :required => true, :desc => "description"
-    def test(word)
-      puts "Test test test"
-    end
 
     desc "activate [language]", "Activate snippets for the [language] language."
     option :all, :type => :boolean, :default => false, :desc => "Deactivate all snippets."
