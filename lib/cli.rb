ca33506cf414115d411f92d201b71dadc125121f
rik0ch3t <rchett02@gmail.com>
2019-11-30 13:13:48 -0800

    implemented exclude

--- lib/cli.rb
+++ lib/cli.rb
@@ -32,6 +32,25 @@ class CLI < Thor
   desc "exclude [language]", "Deactivate snippets for the [language] language."
   option :project, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
   def exclude(language)
-    # TODO
+    if language != 'ruby'
+      puts "Error: Language not supported."
+      return 1
+    end
+
+    if options[:project].nil?
+      puts "Error: Project type not provided."
+      return 1
+    end
+
+    # creat .vscode if it does not exist
+    vscode_path = File.join(Dir.pwd, ".vscode/#{options[:project]}.code-snippets")
+    if !vscode_path
+      puts "Error: .vscode does not exist."
+      return 1
+    end
+
+    # copy selected projects snippets to .vscode
+    snip_files_path = File.join(Dir.pwd, "projects/#{options[:project]}/.")
+    FileUtils.rm_r(vscode_path)
   end
 end
