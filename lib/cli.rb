0437d52fdb32b1c1f457d312f0c5767ab48deca3
rik0ch3t <rchett02@gmail.com>
2019-11-30 18:00:55 -0800

    refactored

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,6 +6,7 @@ require 'colorize'
 # Main CLI app
 class CLI < Thor
 
+  @@vscode_path = File.join(Dir.pwd, ".vscode")
   @@supported_lang = {
     "ruby" => ["thor", "metasploit", "sinatra"],
     "python" => [],
@@ -16,28 +17,10 @@ class CLI < Thor
   option :lang, :aliases => ["-l"], :type => :string, :required => true, :desc => "The language specific snippets to use."
   option :proj, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
   def include()
-    if !@@supported_lang.has_key?(options[:lang])
-      puts "Error: '#{options[:lang].capitalize}' not supported.".colorize(:red)
-      return 1
-    end
-
-    if !@@supported_lang[options[:lang]].include? options[:proj]
-      puts "Error: '#{options[:proj].capitalize}' snippets unavailable.".colorize(:red)
-      return 1
-    end
-
-    # creat .vscode if it does not exist
-    vscode_path = File.join(Dir.pwd, ".vscode")
-    if !vscode_path
-      puts "Creating directory `.vscode` at #{vscode_path}...".colorize(:yellow)
-      Dir.mkdir(File.join(Dir.pwd, ".vscode"))
-    end
-
-    # copy selected projects snippets to .vscode
-    snip_files_path = File.join(Dir.pwd, "projects/#{options[:proj]}/.")
     puts "Adding files to the `.vscode` directory...".colorize(:yellow)
-    FileUtils.cp_r(snip_files_path, vscode_path)
-
+    validate_input(options[:lang], options[:proj])
+    create_vscode_dir()
+    copy_file_to_vscode(options[:proj])
     puts "Done!".colorize(:green)
   end
 
@@ -45,25 +28,43 @@ class CLI < Thor
   option :lang, :aliases => ["-l"], :type => :string, :required => true, :desc => "The language specific snippets to use."
   option :proj, :aliases => ["-p"], :type => :string, :required => true, :desc => "The project specific snippets to use."
   def exclude()
-    if !@@supported_lang.has_key?(options[:lang])
-      puts "Error: '#{options[:lang].capitalize}' not supported.".colorize(:red)
-      return 1
-    end
-
-    if !@@supported_lang[options[:lang]].include? options[:proj]
-      puts "Error: '#{options[:proj].capitalize}' snippets unavailable.".colorize(:red)
-      return 1
-    end
-
-    # creat .vscode if it does not exist
-    vscode_path = File.join(Dir.pwd, ".vscode/#{options[:proj]}.code-snippets")
-    if !vscode_path
-      puts "Error: .vscode does not exist.".colorize(:red)
-      return 1
-    end
-
-    # copy selected projects snippets to .vscode
-    snip_files_path = File.join(Dir.pwd, "projects/#{options[:proj]}/.")
-    FileUtils.rm_r(vscode_path)
+    validate_input(options[:lang], options[:proj])
+    remove_file_from_vscode(options[:proj])
   end
+
+  private
+
+  # Validates the language and project
+  # @params lang String, proj String
+  def validate_input(lang, proj)
+    if !@@supported_lang.has_key?(lang)
+      puts "Error: '#{lang.capitalize}' not supported.".colorize(:red)
+      return 1
+    end
+
+    if !@@supported_lang[lang].include? proj
+      puts "Error: '#{proj.capitalize}' snippets unavailable.".colorize(:red)
+      return 1
+    end
+  end
+
+  # Creates a .vscode directory if it does not exist.
+  def create_vscode_dir()
+    if !@@vscode_path
+      puts "Creating directory `.vscode` at #{vscode_path}...".colorize(:yellow)
+      Dir.mkdir(File.join(Dir.pwd, ".vscode"))
+    end
+  end
+
+  # Copy selected project snippets to .vscode
+  def copy_file_to_vscode(proj)
+    snip_files_path = File.join(Dir.pwd, "projects/#{proj}/.")
+    FileUtils.cp_r(snip_files_path, @@vscode_path)
+  end
+
+  # Remove selected project snippets from .vscode
+  def remove_file_from_vscode(proj)
+    FileUtils.rm_r(@@vscode_path)
+  end
+
 end
